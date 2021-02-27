f32bb97f198df7515af0be84a7fa6f923989106c
marco <marco@MarcosTolentino.mshome.net>
2021-02-27 00:37:20 -0300

    Separado o required do conteudo do index

--- index.php
+++ index.php
@@ -46,31 +46,7 @@
     <? } ?>
     <BR>
     <?
-    //Model
-    $FileModel = 'Models/' . CLASSE . 'Model.php';
-    if (file_exists($FileModel)) {
-        require_once 'Models/Model.php';
-        require_once $FileModel;
-    }
-
-    //Controller
-    $FileControler = 'Controllers/' . CLASSE . '.php';
-    if (file_exists($FileControler)) {
-        require_once 'Controllers/Controller.php';
-        require_once $FileControler;
-    }
-
-    //Classe
-    $Classe = CLASSE;
-    if (class_exists($Classe)) {
-        $Classe = new $Classe();
-
-        //Metodo
-        $Metodo = METODO;
-        if (method_exists($Classe, $Metodo)) {
-            $Classe->$Metodo();
-        }
-    }
+    require_once 'conteudo.php'
     //Não existe classe
     else {
         echo '<h2>1ª Cadastre os cursos</h2>';
